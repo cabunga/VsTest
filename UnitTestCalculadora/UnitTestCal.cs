bd72e077a92ece448bd16d96908743489186db76
admin <admin@admin.com>
2016-02-14 21:40:39 -0500

    first commit

---
+++ UnitTestCalculadora/UnitTestCal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VSTest;
+
+namespace UnitTestCalculadora
+{
+    [TestClass]
+    public class UnitTestCal
+    {
+        [TestMethod]
+        public void TestMethodSuma()
+        {
+            Calculadora calculadora = new Calculadora();
+            calculadora.Numero1 = 2;
+            calculadora.Numero2 = 3;
+            Assert.AreEqual(8, calculadora.Suma());
+        }
+    }
+}
---
+++ VSTest/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSTest
+{
+    public class Calculadora
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+        public int Suma()
+        {
+            return Numero1 + Numero2;
+        }
+        public int Resta()
+        {
+            return Numero1 - Numero2;
+        }
+
+    }
+}
